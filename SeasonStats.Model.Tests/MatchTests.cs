696ebf67c2639a0fb819c47fd8f9b6654becad17
astro6703 <astro6703@gmail.com>
2018-07-10 16:31:56 +0300

    Added numer of sets restriction

--- SeasonStats.Model.Tests/MatchTests.cs
+++ SeasonStats.Model.Tests/MatchTests.cs
@@ -7,56 +7,52 @@ namespace SeasonStats.Model.Tests
     public class MatchTests
     {
         [Fact]
-        public static void AddingSetWithDifferentPlayersThrowsException()
+        public void AddingSetWithDifferentPlayersThrowsException()
         {
             Player player1 = new Player("1");
             Player player2 = new Player("2");
             Player player3 = new Player("3");
-            Match match = new Match();
+            Match match = new Match(5);
             match.AddSet(new Set(player1, player2, 11, 2));
             match.AddSet(new Set(player1, player2, 11, 2));
             Assert.Throws<Exception>(() => match.AddSet(new Set(player1, player3, 2, 11)));
         }
 
         [Fact]
-        public static void AddingMoreThanSevenSetsThrowsException()
-        {
-            Player player1 = new Player("Gleb");
-            Player player2 = new Player("NotGleb");
-            Match match = new Match();
-            match.AddSet(new Set(player1, player2, 11, 2));
-            match.AddSet(new Set(player1, player2, 11, 2));
-            match.AddSet(new Set(player1, player2, 11, 2));
-            match.AddSet(new Set(player1, player2, 11, 2));
-            match.AddSet(new Set(player1, player2, 1, 11));
-            match.AddSet(new Set(player1, player2, 1, 11));
-            match.AddSet(new Set(player1, player2, 1, 11));
-            Assert.Throws<Exception>(() => match.AddSet(new Set(player1, player2, 1, 11)));
-        }
-
-        [Fact]
-        public static void AddingNullSetThrowsArgumentNullException()
+        public void AddingNullSetThrowsArgumentNullException()
         {
             Set set = null;
-            Match match = new Match();
+            Match match = new Match(3);
             Assert.Throws<ArgumentNullException>(() => match.AddSet(set));
         }
 
         [Fact]
-        public static void CountFinalScore()
+        public void CountFinalScore()
         {
             Player player1 = new Player("Gleb");
             Player player2 = new Player("NotGleb");
-            Match match = new Match();
-            match.AddSet(new Set(player1, player2, 11, 2));
+            Match match = new Match(7);
             match.AddSet(new Set(player1, player2, 11, 2));
             match.AddSet(new Set(player1, player2, 11, 2));
             match.AddSet(new Set(player1, player2, 11, 2));
             match.AddSet(new Set(player1, player2, 1, 11));
             match.AddSet(new Set(player1, player2, 1, 11));
-
+            match.AddSet(new Set(player1, player2, 1, 11));
+            match.AddSet(new Set(player1, player2, 11, 1));
             Assert.Equal(4, match.Player1Score);
-            Assert.Equal(2, match.Player2Score);
+            Assert.Equal(3, match.Player2Score);
+        }
+
+        [Fact]
+        public void ThrowsExceptonWhenAddingSetToAFinishedMatch()
+        {
+            Player player1 = new Player("Gleb");
+            Player player2 = new Player("NotGleb");
+            Match match = new Match(5);
+            match.AddSet(new Set(player1, player2, 11, 2));
+            match.AddSet(new Set(player1, player2, 11, 2));
+            match.AddSet(new Set(player1, player2, 11, 2));
+            Assert.Throws<Exception>(() => match.AddSet(new Set(player1, player2, 11, 2)));
         }
     }
 }
--- SeasonStats.Model/Match.cs
+++ SeasonStats.Model/Match.cs
@@ -8,7 +8,15 @@ namespace SeasonStats.Model
     {
         public Player Player1 { get; set; }
         public Player Player2 { get; set; }
+        public int MaximalNumberOfSets { get; }
         private List<Set> sets = new List<Set>();
+
+        public Match(int MaxSets)
+        {
+            if (MaxSets != 3 && MaxSets != 5 && MaxSets != 7) throw new ArgumentException();
+
+            MaximalNumberOfSets = MaxSets;
+        }
 
         public int Player1Score => sets.Count(set => set.Player1Score > set.Player2Score);
         public int Player2Score => sets.Count(set => set.Player2Score > set.Player1Score);
@@ -17,13 +25,14 @@ namespace SeasonStats.Model
         {
             if (set == null) throw new ArgumentNullException();
             if (!set.IsValid()) throw new Exception("Given set is not valid");
-            if (sets.Count == 7) throw new Exception("Number of sets in match has been exceeded");
             if (sets.Count == 0)
             {
                 Player1 = set.Player1;
                 Player2 = set.Player2;
             }
             if (!set.Player1.Equals(Player1) || !set.Player2.Equals(Player2)) throw new Exception("Match players doesn't match to match players");
+            if ((Player1Score == (MaximalNumberOfSets + 1) / 2) || Player1Score == (MaximalNumberOfSets + 1) / 2)
+                throw new Exception("The match has already got a winner");
 
             sets.Add(set);
         }
