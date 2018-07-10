a93a260f1bae51cae47bf226b97d871b8d4635d6
astro6703 <astro6703@gmail.com>
2018-07-10 15:59:55 +0300

    Added validation

--- SeasonStats.Model/Match.cs
+++ SeasonStats.Model/Match.cs
@@ -6,18 +6,24 @@ namespace SeasonStats.Model
 {
     public class Match
     {
-        public Player Player1 { get; }
-        public Player Player2 { get; }
-        private List<Set> sets;
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        private List<Set> sets = new List<Set>();
 
         public int Player1Score => sets.Count(set => set.Player1Score > set.Player2Score);
         public int Player2Score => sets.Count(set => set.Player2Score > set.Player1Score);
 
         public void AddSet(Set set)
         {
-            if (!set.Player1.Equals(Player1) || !set.Player2.Equals(Player2)) throw new Exception("Set players doesn't match to match players");
-            if (sets.Count >= 7) throw new Exception("Number of sets in match has been exceeded");
             if (set == null) throw new ArgumentNullException();
+            if (!set.IsValid()) throw new Exception("Given set is not valid");
+            if (sets.Count == 7) throw new Exception("Number of sets in match has been exceeded");
+            if (sets.Count == 0)
+            {
+                Player1 = set.Player1;
+                Player2 = set.Player2;
+            }
+            if (!set.Player1.Equals(Player1) || !set.Player2.Equals(Player2)) throw new Exception("Match players doesn't match to match players");
 
             sets.Add(set);
         }
--- SeasonStats.Model/Set.cs
+++ SeasonStats.Model/Set.cs
@@ -13,11 +13,21 @@ namespace SeasonStats.Model
 
         public Set(Player player1, Player player2, int player1Score, int player2Score)
         {
-            if (player1Score < 0 && player1Score > 11 && player2Score < 0 && player2Score > 11) throw new ArgumentException("Invalid score");
+            if (player1Score < 0 || player2Score < 0) throw new ArgumentException("Invalid score");
             if (player1 == null || player2 == null) throw new ArgumentNullException();
 
+            Player1 = player1;
+            Player2 = player2;
             Player1Score = player1Score;
             Player2Score = player2Score;
+        }
+
+        public bool IsValid()
+        {
+            if (((Player1Score == 11 ^ Player2Score == 11) && Math.Abs(Player1Score - Player2Score) >= 2) ||
+                (Player1Score >= 10 && Player2Score >= 10 && Math.Abs(Player1Score - Player2Score) == 2))
+                return true;
+            else return false;
         }
     }
 }
